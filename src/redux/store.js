99440a63c7bae404f5d7f5a290f5847088bc8ce8
Aravindan-111 <aravindhan11195@gmail.com>
2021-12-23 15:11:13 +0530

    persist added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,27 @@
 import * as redux from "redux";
 import Reducer from "./Reducer";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/lib/persistReducer";
+import persistStore from "redux-persist/lib/persistStore";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 
 const root = redux.combineReducers({ cart: Reducer });
 
-export const store = redux.createStore(root);
+const persistConfig = {
+  key: "local data",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: "aravind!@dE",
+      onError: function (error) {
+        console.log(error);
+      },
+    }),
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig, root);
+
+export const store = redux.createStore(persistedReducer);
+
+export const persistor = persistStore(store);
