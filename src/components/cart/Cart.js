8afbba4a165805f1f2afd91fcb91722eefe21035
Aravindan-111 <aravindhan11195@gmail.com>
2021-12-23 14:43:58 +0530

    basket math done

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,12 +4,21 @@ import "./Cart.css";
 
 function Cart() {
   const cart = useSelector((state) => state.cart.cart);
-  console.log(cart);
   const dispatch = useDispatch();
+  console.log(cart);
+
+  const itemPrice = cart.reduce(
+    (acc, current) => acc + current.price * current.quantity,
+    0
+  );
+  const tax = (itemPrice * 0.12).toFixed(2);
+  const shippingCost = itemPrice > 1000 ? 0 : 50;
+  const totalCost = Math.round(+itemPrice + +tax + +shippingCost);
 
   const incCart = (item) => {
     dispatch({ type: "incCart", payload: item });
   };
+
   const decCart = (item) => {
     item.quantity > 1
       ? dispatch({ type: "decCart", payload: item })
@@ -43,19 +52,19 @@ function Cart() {
 
         <div className="nameValue">
           <h4>Item Price :</h4>
-          <h4>98</h4>
+          <h4>{itemPrice}</h4>
         </div>
         <div className="nameValue">
           <h4>Tax Price : </h4>
-          <h4>98</h4>
+          <h4>{tax}</h4>
         </div>
         <div className="nameValue">
           <h4>Shipping Price : </h4>
-          <h4>98</h4>
+          <h4>{shippingCost}</h4>
         </div>
         <div className="nameValue">
           <h2>Total : </h2>
-          <h2>98</h2>
+          <h2>{totalCost}</h2>
         </div>
 
         <hr></hr>
